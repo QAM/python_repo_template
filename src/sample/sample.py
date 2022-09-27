c4a7b2e8de92312a485c432d7ef8ff85e127c62a
liangyu-outreach <liangyu.chen@outreach.io>
2022-09-27 15:31:33 -0700

    fix

--- src/sample/sample.py
+++ src/sample/sample.py
@@ -1,8 +1,22 @@
-def very_long_function(
+#
+# This is sample code
+#
+
+
+def test_func(
     long_variable_name,
     long_variable_name2,
     long_variable_name3,
     long_variable_name4,
     long_variable_name5,
 ):
+    """test func
+
+    Args:
+        long_variable_name (_type_): _description_
+        long_variable_name2 (_type_): _description_
+        long_variable_name3 (_type_): _description_
+        long_variable_name4 (_type_): _description_
+        long_variable_name5 (_type_): _description_
+    """
     pass
