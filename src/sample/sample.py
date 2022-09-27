65f1af941f9fdb4ec4403418f27d7f6f78759cdb
liangyu-outreach <liangyu.chen@outreach.io>
2022-09-27 15:33:48 -0700

    add return for sample func

--- src/sample/sample.py
+++ src/sample/sample.py
@@ -18,5 +18,8 @@ def test_func(
         long_variable_name3 (_type_): _description_
         long_variable_name4 (_type_): _description_
         long_variable_name5 (_type_): _description_
+
+    Returns:
+        boolean: _description_
     """
-    pass
+    return True
